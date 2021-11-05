be936932d4827028492e94c8853068c26e8c9c87
Karolina Mizgier <karolinamizgier@gmail.com>
2021-11-05 16:47:13 +0100

    First steps

--- src/main/java/com/kodilla/ships/ComputerBoard.java
+++ src/main/java/com/kodilla/ships/ComputerBoard.java
@@ -21,23 +21,21 @@ public class ComputerBoard {
         for (Node node : excluded) {
             int n= GridPane.getRowIndex(node);
             int m = GridPane.getColumnIndex(node);
-            if (!(n == x || m == y || n == x + 1 || n == x - 1 || n == x - 2 || n == x + 2 || m == y + 1 || m == y - 1 || m == y - 2 || m == y + 2)) {
+            if ((n == x) || (m == y) || (n == (x + 1)) || (n == (x - 1)) || (n == (x - 2)) || (n == (x + 2)) || (m == (y + 1)) || (m == (y - 1)) || (m == (y - 2)) || (m == (y + 2))) {
+                nextRandom(excluded);
+            } else {
                 l.add(x);
                 l.add(y);
-            } else {
-                x = randomInt(1, 10);
-                y = randomInt(1, 10);
             }
         }
         return l;
     }
 
-
     public GridPane setBoard(GridPane grid2) {
         ButtonExtractor buttonEx = new ButtonExtractor();
         List<Node> excluded = new ArrayList<>();
-        int temp = randomInt(1, 10);
-        int temp2 = randomInt(1, 10);
+        int temp = randomInt(1,10);
+        int temp2 = randomInt(1,10);
         Node n1 = buttonEx.getNodeByRowCol(grid2, temp, temp2);
         Node n2 = buttonEx.getNodeByRowCol(grid2, temp + 1, temp2);
         n1.setUserData("x");
@@ -55,31 +53,10 @@ public class ComputerBoard {
         excluded.add(n3);
         excluded.add(n4);
 
-        temps.removeAll(temps);
-        temps = nextRandom(excluded);
-        int temp5 = temps.get(0);
-        int temp6 = temps.get(1);
-        Node n5 = buttonEx.getNodeByRowCol(grid2, temp5, temp6);
-        Node n6 = buttonEx.getNodeByRowCol(grid2, temp5, temp6+1);
-        n5.setUserData("x");
-        n6.setUserData("x");
-        excluded.add(n5);
-        excluded.add(n6);
 
-        temps.removeAll(temps);
-        temps = nextRandom(excluded);
-        int temp7 = temps.get(0);
-        int temp8 = temps.get(1);
-        Node n7 = buttonEx.getNodeByRowCol(grid2, temp7, temp8);
-        Node n8 = buttonEx.getNodeByRowCol(grid2, temp7, temp8+1);
-        n7.setUserData("x");
-        n8.setUserData("x");
-        excluded.add(n7);
-        excluded.add(n8);
 
 
         return grid2;
-
     }
 }
 
